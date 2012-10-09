0c25ffe9d66a364c79d9e4b131e85a93d3aaaf18
egisz <egidijus@unicall.lt>
2012-10-09 12:47:30 +0300

    Fixed time tracker links from absolute to path_to... to avoid broken links when application is deployed in virtual folder

--- assets/javascripts/time_tracker.js
+++ assets/javascripts/time_tracker.js
@@ -5,17 +5,15 @@ function updateElementIfChanged(data) {
 function updateTimeTrackerMenu() {
   var project_id = $('#time_tracker_info').data('project-id');
   var issue_id = $('#time_tracker_info').data('issue-id');
-  $.get('/time_trackers/render_menu', {project_id:project_id, issue_id:issue_id}, updateElementIfChanged);
+  var path = $('div#time_tracker_info').data('time-trackers-path');
+  $.get(path+'/render_menu', {project_id:project_id, issue_id:issue_id}, updateElementIfChanged);
 }
 
 //This script periodically updates the time tracker menu item to reflect any changes in the tracking state
 //Refresh rate is taken from settings. If settings is invalid, 60 secs is used. The minimum value is 5 secs.
 function runTimeTrackerUpdater() {
   var refresh_rate = $('#time_tracker_info').data('refresh-rate');
-  refresh_rate = parseInt(refresh_rate) || 0;
-  if (refresh_rate == 0) {
-    refresh_rate = Math.max(5, refresh_rate);
-  }
+  refresh_rate = parseInt(refresh_rate) || 60;
   setInterval(updateTimeTrackerMenu, refresh_rate * 1000);
 }
 
@@ -24,7 +22,6 @@ function renderMenu() {
   $('#time-tracker-menu').replaceWith(misplaced_menu_html.html());
   misplaced_menu_html.remove()
 }
-
 
 $(function () {
   renderMenu();
