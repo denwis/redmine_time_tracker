cc6364a7cf84850307d65c26e6894ef6166ec443
egisz <egidijus@unicall.lt>
2012-10-11 16:58:12 +0300

    Fixed minor error

--- assets/javascripts/time_tracker.js
+++ assets/javascripts/time_tracker.js
@@ -1,32 +1,37 @@
 function updateElementIfChanged(data) {
-  $('#time-tracker-menu').html(data);
+    $('#time-tracker-menu').html(data);
 }
 
 function updateTimeTrackerMenu() {
-  var project_id = $('#time_tracker_info').data('project-id');
-  var issue_id = $('#time_tracker_info').data('issue-id');
-  var path = $('div#time_tracker_info').data('time-trackers-path');
-  $.get(path+'/render_menu', {project_id:project_id, issue_id:issue_id}, updateElementIfChanged);
+    var project_id = $('#time_tracker_info').data('project-id');
+    var issue_id = $('#time_tracker_info').data('issue-id');
+    var path = $('div#time_tracker_info').data('time-trackers-path');
+    if (path) {
+        $.get(path+'/render_menu', {
+            project_id:project_id,
+            issue_id:issue_id
+        }, updateElementIfChanged);
+    }
 }
 
 //This script periodically updates the time tracker menu item to reflect any changes in the tracking state
 //Refresh rate is taken from settings. If settings is invalid, 60 secs is used. The minimum value is 5 secs.
 function runTimeTrackerUpdater() {
-  var refresh_rate = $('#time_tracker_info').data('refresh-rate');
-  refresh_rate = parseInt(refresh_rate) || 60;
-  setInterval(updateTimeTrackerMenu, refresh_rate * 1000);
+    var refresh_rate = $('#time_tracker_info').data('refresh-rate');
+    refresh_rate = parseInt(refresh_rate) || 60;
+    setInterval(updateTimeTrackerMenu, refresh_rate * 1000);
 }
 
 function renderMenu() {
-  var misplaced_menu_html = $('#misplaced-menu');
-  $('#time-tracker-menu').replaceWith(misplaced_menu_html.html());
-  misplaced_menu_html.remove()
+    var misplaced_menu_html = $('#misplaced-menu');
+    $('#time-tracker-menu').replaceWith(misplaced_menu_html.html());
+    misplaced_menu_html.remove()
 }
 
 $(function () {
-  renderMenu();
-  updateTimeTrackerMenu();
-  runTimeTrackerUpdater();
+    renderMenu();
+    updateTimeTrackerMenu();
+    runTimeTrackerUpdater();
 })
 
 // Used in Plugin Settings page to delete transition
@@ -47,13 +52,13 @@ function addTransitionField() {
 
 // Add contextual button to issue for starting task.
 $(document).ready( function() {
-      var areas = $('div#content>div.contextual').find('a.icon-time-add');
-      var startButtonTemplate = $('a.time-tracker-start')[0];
-	for (ai = 0; ai < areas.length; ai++) {
-		var area = areas[ai];
-		var startButton = startButtonTemplate.cloneNode(true);
-		startButton.style.display = 'inline';
-		$(startButton).insertBefore(area);
-	}
+    var areas = $('div#content>div.contextual').find('a.icon-time-add');
+    var startButtonTemplate = $('a.time-tracker-start')[0];
+    for (ai = 0; ai < areas.length; ai++) {
+        var area = areas[ai];
+        var startButton = startButtonTemplate.cloneNode(true);
+        startButton.style.display = 'inline';
+        $(startButton).insertBefore(area);
+    }
 });
 
