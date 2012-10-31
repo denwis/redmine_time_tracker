7f92d6515239f85e4aa59d82dfd76fb4c0405ae2
egisz <egidijus@unicall.lt>
2012-10-31 16:21:22 +0200

    db migration to remove old duplicate timers. Add unique contstraint on time_trackers table

---
+++ db/migrate/005_delete_duplicates.rb
@@ -0,0 +1,13 @@
+class DeleteDuplicates < ActiveRecord::Migration
+  def self.up
+    latest = TimeTracker.find(:all, :select => :id, :order => "started_on DESC", :group => "user_id")
+    TimeTracker.delete_all(["id NOT IN (?)", latest])
+
+    add_index :time_trackers, :user_id, :unique => true
+  end
+
+  def self.down
+      # not much we can do to restore deleted data
+    raise ActiveRecord::IrreversibleMigration, "Can't recover the deleted time trackers"
+  end
+end
